fe6bf277b7ea8b44c5aed5be77e2b01e1d0256aa
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-02-04 19:56:56 -0300

    codepix api bank

---
+++ bank-api/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BankAccount } from './models/bank-account.model';
+import { BankAccountController } from './controllers/bank-account/bank-account.controller';
+// decorator está presente no ES7
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [BankAccount]
+    }),
+    TypeOrmModule.forFeature([BankAccount])
+  ],
+  controllers: [AppController, BankAccountController],
+  providers: [AppService],
+})
+export class AppModule {}
